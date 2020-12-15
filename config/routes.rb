cf8092c5f053488238ea7247b58b137f813cfa0c
kazuomi-nakajima <x15076.nakajima@gmail.com>
2020-12-15 18:20:46 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'rooms/new'
+  root to:"rooms#index"
   devise_for :users
-  get 'messages/index'
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
-  get 'rooms/new'
-
-  root to:"messages#index"
 end
